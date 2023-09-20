a96e53ad9d311f6ce72801994ae740254e3ec088
Robert-Card <robertleandroc@gmail.com>
2023-09-20 01:26:21 -0300

    QoL and UI Updates

--- script.js
+++ script.js
@@ -6,26 +6,36 @@ function Redo() { document.execCommand("redo", false, null); }
 
 
 function Clear() {
-    let erase = confirm("Are you sure? *it cannot be undone!");
-    if(erase == true){
-
+    
         document.getElementById("gen").value=""; 
 
-    }else{
-
-    }    
 }
 
 function Copy() {
+    
+    if(document.getElementById("gen").value==""){
+        
+        notification.innerText = "Empty TextBox!";
+        notification.style.backgroundColor = "#800000";
+        showNotification();
 
-    document.querySelector("textarea").select();
-    document.execCommand('copy');
+    }else{
+        document.querySelector("textarea").select();
+        document.execCommand('copy');
+
+        notification.innerText = "Copied!";
+        notification.style.backgroundColor = "#7499f09d";
+        showNotification();
+    }
 }
 
 function Transform() {
 
     if(document.getElementById("gen").value==""){
-        alert("Error, empty TextBox!");
+        
+        notification.innerText = "Empty TextBox!";
+        notification.style.backgroundColor = "#800000";
+        showNotification();
 
     }else{
 
@@ -37,6 +47,10 @@ function Transform() {
         document.getElementById("gen").value="[ "; 
         splits.forEach(Divide); 
         document.getElementById("gen").value+=" ]";
+
+        notification.innerText = "Transformed!";
+        notification.style.backgroundColor = "#7499f09d";
+        showNotification();
     }
 }
 
@@ -46,4 +60,15 @@ function Divide(item, index, array){
     if(index !== array.length -1){
     document.getElementById("gen").value+=", ";
     }
+}
+
+function showNotification() {
+    
+    const notification = document.getElementById("notification");
+
+    notification.style.display = "block";
+
+    setTimeout(function () {
+        notification.style.display = "none";
+    }, 1000); 
 }
